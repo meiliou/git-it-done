6480cb3156525626e59ca5d2f117943d41d5d0a8
meiliou <83768277+meiliou@users.noreply.github.com>
2023-03-08 17:08:55 +0200

    created elements, added error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,6 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
 
 var getReposIssues = function(repo) {
     console.log(repo);
@@ -6,18 +8,21 @@ var getReposIssues = function(repo) {
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
-          response.json().then(function(data) {
-            // pass response data to dom function
-            displayIssues(data);
-        });
+            response.json().then(function(data) {
+                displayIssues(data);
+          
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo)
+                }
+            });
+        } else {
+            alert("There was a problem with your request!");
         }
-        else {
-          alert("There was a problem with your request!");
-        }
-      });
-    };
+    });
+};
 
-getReposIssues("meiliou/run-buddy");
+getReposIssues("facebook/react");
 
 var displayIssues = function(issues) {
     if (issues.length === 0) {
@@ -58,4 +63,16 @@ var displayIssues = function(issues) {
     };
 };
 
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
 
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+  
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+
+  };
