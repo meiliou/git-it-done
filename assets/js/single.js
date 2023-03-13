7f3d710529659d93cfce517e06295d58b7b93461
meiliou <83768277+meiliou@users.noreply.github.com>
2023-03-13 08:25:58 +0200

    link single repo page

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,28 +1,45 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
 
-var getReposIssues = function(repo) {
-    console.log(repo);
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+  
+    if (repoName) {
+      // display repo name on the page
+      repoNameEl.textContent = repoName;
+  
+      getRepoIssues(repoName);
+    } else {
+      // if no repo was given, redirect to the homepage
+      document.location.replace("./index.html");
+    }
+  };
+
+var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
             response.json().then(function(data) {
-                displayIssues(data);
-          
-                // check if api has paginated issues
-                if (response.headers.get("Link")) {
-                    displayWarning(repo)
-                }
-            });
+            displayIssues(data);
+    
+            // check if api has paginated issues
+            if (response.headers.get("Link")) {
+            displayWarning(repo);
+            }
+        });
         } else {
-            alert("There was a problem with your request!");
+        // if not successful, redirect to homepage
+        document.location.replace("./index.html");
         }
     });
 };
 
-getReposIssues("facebook/react");
 
 var displayIssues = function(issues) {
     if (issues.length === 0) {
@@ -76,3 +93,6 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 
   };
+
+  getRepoName();
+
